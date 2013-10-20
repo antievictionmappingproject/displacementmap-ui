f6816b84a1818297f5f1b3d783011269b45ba698
Robert Christian <rchristian@liaison.com>
2013-10-20 10:03:47 -0700

    major upgrades incl bootstrap3

--- app/js/app.js
+++ app/js/app.js
@@ -2,17 +2,22 @@
 
 // declare top-level module which depends on filters,and services
 var myApp = angular.module('myApp',
-    ['myApp.filters',
+    [   'myApp.filters',
         'myApp.directives', // custom directives
         'ngGrid', // angular grid
         'ui', // angular ui
         'ngSanitize', // for html-bind in ckeditor
+        'ui.ace', // ace code editor
         'ui.bootstrap', // jquery ui bootstrap
         '$strap.directives' // angular strap
     ]);
 
+
+var filters = angular.module('myApp.filters', []);
+var directives = angular.module('myApp.directives', []);
+
 // bootstrap angular
-myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+myApp.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
 
     // TODO use html5 *no hash) where possible
     // $locationProvider.html5Mode(true);
@@ -20,11 +25,20 @@ myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $
     $routeProvider.when('/', {
         templateUrl:'partials/home.html'
     });
+
+    $routeProvider.when('/fs2', {
+        templateUrl:'partials/fs2/fs2.html',
+        controller: 'FS2Controller'
+    });
+
     $routeProvider.when('/contact', {
         templateUrl:'partials/contact.html'
     });
     $routeProvider.when('/about', {
         templateUrl:'partials/about.html'
+    });
+    $routeProvider.when('/faq', {
+        templateUrl:'partials/faq.html'
     });
 
     // note that to minimize playground impact on app.js, we
@@ -65,15 +79,14 @@ myApp.run(function ($rootScope, $location, $http, $timeout, AuthService, RESTSer
         }
     );
 
-
     // *****
     // Initialize authentication
     // *****
     $rootScope.authService = AuthService;
 
     // text input for login/password (only)
-    $rootScope.loginInput = 'git@github.com';
-    $rootScope.passwordInput = 'git';
+    $rootScope.loginInput = 'user@gmail.com';
+    $rootScope.passwordInput = 'complexpassword';
 
     $rootScope.$watch('authService.authorized()', function () {
 
@@ -83,20 +96,32 @@ myApp.run(function ($rootScope, $location, $http, $timeout, AuthService, RESTSer
             return;
         }
 
+        // instantiate and initialize an auth notification manager
+        $rootScope.authNotifier = new NotificationManager($rootScope);
+
         // when user logs in, redirect to home
         if ($rootScope.authService.authorized()) {
             $location.path("/");
+            $rootScope.authNotifier.notify('information', 'Welcome ' + $rootScope.authService.currentUser() + "!");
         }
 
         // when user logs out, redirect to home
         if (!$rootScope.authService.authorized()) {
             $location.path("/");
+            $rootScope.authNotifier.notify('information', 'Thanks for visiting.  You have been signed out.');
         }
 
     }, true);
 
+    // TODO move this out to a more appropriate place
+    $rootScope.faq = [
+        {key: "What is the service-nucleus?", value: "The service nucleus is a starting point for a full-blown Java webservice and accompanying UI."},
+        {key: "What are the pre-requisites for running the service nucleus?", value: "You need JDK 7 and Gradle>=1.6."},
+        {key: "How do I change styling (css)?", value:  "See service-implementation/bootstrap.  First change the less modules, then compile using build.sh.  The resulting artifacts will be copied to the appropriate location."},
+        {key: "How do I implement a REST service?", value:  "Simply add a new Jersey Resource.  See service-implementation/src/main/java/com/liaison/service/resources/examples for examples."},
+        {key: "How do I brand my project (rename from Hello-World)?", value: "This is currently a manual process with about a half-dozen steps.  See README.md for details."},
+        {key: "How do I expose JMX metrics?", value: "Checkout the MetricsResource example."}
+    ];
+
+
 });
-
-
-
-
--- bootstrap/build.sh
+++ bootstrap/build.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
+# prereqs to build... npm, etc  (on mac)
+# =======================================
+# brew install node
+# make sure make is installed - mac instructions: http://stackoverflow.com/questions/10265742/how-to-install-make-and-gcc-on-a-mac
+# git clone http://github.com/isaacs/npm.git
+# cd npm
+# sudo make install
+# now you have node package manager (npm)
+# sudo npm install less -g
+# sudo npm install uglify-js -g
+# sudo npm install recess -g
+# ... also npm install grunt and jslint
+# install ruby gem and then gem install hekyll
+
+# cd to this directory (bootstrap)
+# finally, run ./build.sh
+
 # build bootstrap to target folder "bootstrap"
-rm -r bootstrap
-make bootstrap
+npm install
+rm -i -r -f dist
+mkdir dist
+grunt dist
 
 # copy built css to app destination
 rm -f -r ../app/css/vendor/bootstrap
 mkdir ../app/css/vendor/bootstrap
-cp bootstrap/css/*.* ../app/css/vendor/bootstrap
+cp dist/css/*.* ../app/css/vendor/bootstrap
 
 # copy built js to app destination
 rm -f -r ../app/js/vendor/bootstrap
 mkdir ../app/js/vendor/bootstrap
-cp bootstrap/js/*.* ../app/js/vendor/bootstrap
+cp dist/js/*.* ../app/js/vendor/bootstrap
+
+# copy fonts to app destination
+rm -f -r ../app/fonts
+mkdir ../app/fonts
+cp fonts/*.* ../app/fonts
